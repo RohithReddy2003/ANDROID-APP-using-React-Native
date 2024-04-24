e851ca64bfd81989badbcb078292b635655910cb
RohithReddy2003 <158015264+RohithReddy2003@users.noreply.github.com>
2024-04-25 00:16:26 +0530

    Update HostEvents.js

--- HostEvents.js
+++ HostEvents.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, Button, TouchableOpacity ,ScrollView} from 'rea
 import axios from 'axios';
 import { useNavigation, useRoute } from '@react-navigation/native';
 
-const URL = 'http://192.168.1.6:5000'; // Define the URL constant
+const URL = ''; // Define the URL constant
 
 const CreatedEventList = ({ route }) => {
   const [events, setEvents] = useState([]);
